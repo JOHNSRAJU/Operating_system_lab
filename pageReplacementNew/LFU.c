b7a65288904713a371d3607400cfddbc3a8fdf44
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2022-10-10 13:24:11 +0530

    Add files via upload

--- pageReplacementNew/LFU.c
+++ pageReplacementNew/LFU.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int n,f,i,j,flag,pag,pageFault,pages[30],result[10];
+    int n,f,i,j,k,min,flag,pag=0,pageFault,pages[30],result[10],freq[10];
     printf("Enter no. of pages :- ");
     scanf("%d",&n);
     printf("Enter no of frames :- ");
@@ -11,6 +11,7 @@ int main(){
     }
     for(i=0;i<f;i++){
         result[i]=-1;
+        freq[i]=0;
     }
     printf("REF \t\tPages\n");
     pageFault=0;
@@ -20,11 +21,22 @@ int main(){
         for(j=0;j<f;j++){
             if(result[j]==pages[i]){
                 flag=1;
+                freq[j]++;
                 break;
             }
         }
         if(flag==0){
-            result[pag]=pages[i];
+            k=pag;
+            min=pag;
+            for(j=0;j<f;j++){
+                if(freq[k]<freq[min]){
+                    min=k;
+                    pag=(pag+1)%f;
+                }
+                k=(k+1)%f;
+            }
+            result[min]=pages[i];
+            freq[min]=1; 
             pag=(pag+1)%f;    
             pageFault++; 
             printf("%d\t\t",pages[i]);      
@@ -35,7 +47,7 @@ int main(){
         for(j=0;j<f;j++){
             if(result[j]!=-1){
                 printf("%d   ",result[j]);
-            }           
+            }            
         }
         printf("\n");
     }
--- pageReplacementNew/LRU.c
+++ pageReplacementNew/LRU.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int n,f,i,j,flag,pag,pageFault,pages[30],result[10];
+    int n,f,i,j,k,flag,pag,pageFault,pages[30],result[10],order[10];
     printf("Enter no. of pages :- ");
     scanf("%d",&n);
     printf("Enter no of frames :- ");
@@ -11,6 +11,7 @@ int main(){
     }
     for(i=0;i<f;i++){
         result[i]=-1;
+        order[i]=-1;
     }
     printf("REF \t\tPages\n");
     pageFault=0;
@@ -20,14 +21,21 @@ int main(){
         for(j=0;j<f;j++){
             if(result[j]==pages[i]){
                 flag=1;
+                order[j]=i;
                 break;
             }
         }
         if(flag==0){
+            pag=0;
+            for(j=0;j<f;j++){
+                if(order[j]<order[pag]){
+                    pag=j;
+                }
+            }
             result[pag]=pages[i];
-            pag=(pag+1)%f;    
-            pageFault++; 
-            printf("%d\t\t",pages[i]);      
+            pageFault++;
+            order[pag]=i;
+            printf("%d \t\t",pages[i]);
         }
         else{
             printf("%d *\t\t",pages[i]);
@@ -35,7 +43,7 @@ int main(){
         for(j=0;j<f;j++){
             if(result[j]!=-1){
                 printf("%d   ",result[j]);
-            }           
+            }          
         }
         printf("\n");
     }
